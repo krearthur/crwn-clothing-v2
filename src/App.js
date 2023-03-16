af037929773805c0c03eaa5dc39aaf734f7449fd
Grohe, Arthur <arthur.grohe@greenique.de>
2023-03-16 11:16:23 +0100

    close cart when clicking anywhere

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import Home from './routes/home/home.component';
-import Navigation from './routes/navigation/navigation.component';
-import Authentication from './routes/authentication/authentication.component';
-import Shop from './routes/shop/shop.component';
-import Checkout from './routes/checkout/checkout.component';
-import { checkUserSession } from './store/user/user.action';
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
+import Checkout from "./routes/checkout/checkout.component";
+import { checkUserSession } from "./store/user/user.action";
+import { setIsCartOpen } from "./store/cart/cart.action";
+import { selectIsCartOpen } from "./store/cart/cart.selector";
 
 const App = () => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
+	const isCartOpen = useSelector(selectIsCartOpen);
 
-  useEffect(() => {
-    dispatch(checkUserSession());
-  }, []);
+	useEffect(() => {
+		dispatch(checkUserSession());
+	}, []);
 
-  return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='shop/*' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path='checkout' element={<Checkout />} />
-      </Route>
-    </Routes>
-  );
+	const closeCartHandler = () => {
+		if (isCartOpen) {
+			console.log("close cart");
+			dispatch(setIsCartOpen(false));
+		}
+	};
+
+	return (
+		<div onClick={closeCartHandler}>
+			<Routes>
+				<Route path="/" element={<Navigation />}>
+					<Route index element={<Home />} />
+					<Route path="shop/*" element={<Shop />} />
+					<Route path="auth" element={<Authentication />} />
+					<Route path="checkout" element={<Checkout />} />
+				</Route>
+			</Routes>
+		</div>
+	);
 };
 
 export default App;
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,29 +1,33 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import {
-  selectCartCount,
-  selectIsCartOpen,
-} from '../../store/cart/cart.selector';
-import { setIsCartOpen } from '../../store/cart/cart.action.js';
+	selectCartCount,
+	selectIsCartOpen,
+} from "../../store/cart/cart.selector";
+import { setIsCartOpen } from "../../store/cart/cart.action.js";
 
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
-import { CartIconContainer, ItemCount } from './cart-icon.styles';
+import { CartIconContainer, ItemCount } from "./cart-icon.styles";
 
 const CartIcon = () => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
+	const cartCount = useSelector(selectCartCount);
+	const isCartOpen = useSelector(selectIsCartOpen);
 
-  const cartCount = useSelector(selectCartCount);
-  const isCartOpen = useSelector(selectIsCartOpen);
+	useEffect(() => {
+		console.log("Cart changed!");
+	}, [cartCount]);
 
-  const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
+	const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
 
-  return (
-    <CartIconContainer onClick={toggleIsCartOpen}>
-      <ShoppingIcon className='shopping-icon' />
-      <ItemCount>{cartCount}</ItemCount>
-    </CartIconContainer>
-  );
+	return (
+		<CartIconContainer onClick={toggleIsCartOpen}>
+			<ShoppingIcon className="shopping-icon" />
+			<ItemCount>{cartCount}</ItemCount>
+		</CartIconContainer>
+	);
 };
 
 export default CartIcon;
